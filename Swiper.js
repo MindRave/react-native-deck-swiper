5fb2e11a516639979041d2b486090a369c5301ae
webraptor <contact@webraptor.eu>
2018-03-22 16:54:47 +0200

    Old bug is back in 1.4.9: photo flickering after swiping #130 refactored pushing of rendercards fixed flickering images bug, FOR GOOOD! :)

--- Swiper.js
+++ Swiper.js
@@ -702,22 +702,15 @@ class Swiper extends Component {
         const swipableCardStyle = this.calculateSwipableCardStyle();
         const renderOverlayLabel = this.renderOverlayLabel();
 
-        renderedCards.push(firstCard ?
-          <Animated.View
-            key={key}
-            style={swipableCardStyle}
-            {...this._panResponder.panHandlers}
-          >
-            {renderOverlayLabel}
-            {stackCard}
-          </Animated.View>
-          :
-          <Animated.View key={key} style={stackCardZoomStyle}>
-            {stackCard}
-          </Animated.View>
-        )
-
-        firstCard = false
+        renderedCards.push(<Animated.View
+          key={key}
+          style={firstCard ? swipableCardStyle : stackCardZoomStyle}
+          {...this._panResponder.panHandlers}
+        >
+          {firstCard ? renderOverlayLabel : null}
+          {stackCard}
+        </Animated.View>);
+        firstCard=false;
       }
     }
 
